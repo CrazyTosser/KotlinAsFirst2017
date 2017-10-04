2219276ef48e546785347076b3db16b70aafb30b
CrazyTosser <666_mickhail@mail.ru>
2017-10-04 21:42:36 +0300

    Fixes and format

--- src/lesson5/task1/Parse.kt
+++ src/lesson5/task1/Parse.kt
@@ -245,6 +245,7 @@ fun mostExpensive(description: String): String {
  * Вернуть -1, если roman не является корректным римским числом
  */
 fun fromRoman(roman: String): Int {
+    if (roman.length == 0) return -1
     val rom = mutableListOf<Pair<Int, String>>(Pair(1000, "M"), Pair(900, "CM"), Pair(500, "D"), Pair(400, "CD"),
             Pair(100, "C"), Pair(90, "XC"), Pair(50, "L"), Pair(40, "XL"), Pair(10, "X"), Pair(9, "IX"),
             Pair(5, "V"), Pair(4, "IV"), Pair(1, "I"))
